9c631cb90c4f08c4461d9993d116a1b95e7592a3
David Wayne Baxter <david.w.baxter@gmail.com>
2014-06-11 23:30:15 -0700

    Updated express from 3 > 4. Updated and refactored for dependencies and middleware where applicable.

--- server.js
+++ server.js
@@ -7,34 +7,26 @@ var morgan  = require('morgan');
 var cookieParser = require('cookie-parser');
 var methodOverride = require('method-override');
 var errorHandler = require('errorhandler');
-
+var session = require('express-session');
 var http = require('http');
 var passport = require('passport');
 var mongoose = require('mongoose');
 var domain = require('domain');
 var expressValidator = require('express-validator');
 var path = require('path');
-
-var csrf = require('csurf');
-var favicon = require('serve-favicon');
+//var csrf = require('csurf');
+//var favicon = require('serve-favicon');
 
 // Configs
 var config_db = require('./secrets/db.mongo');
 var config_host = require('./secrets/env.host');
-var sessionStore = require('./secrets/db.sessionStore');
-//var MongoStore = require('connect-mongo')(express);
 
-/*mongoose.connect(config_db.connectionString);
+mongoose.connect(config_db.connectionString);
 mongoose.connection.on('error', function () {
     console.error('✗ Mongo Connection Error. Please make sure Mongo is running.');
 });
-*/
 
 var app = express();
-var hour = 3600000;
-var day = (hour * 24);
-var week = (day * 7);
-var month = (day * 30);
 
 /** Passport Settings **/
 app.use(passport.initialize());
@@ -44,9 +36,8 @@ app.use(function (req, res, next) {
     next();
 });
 
-/** Express Settings */
+/** Express Settings **/
 app.set('port', config_host.port || 5000);
-//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(compression());
 app.use(morgan('dev'));
@@ -58,6 +49,8 @@ app.use(methodOverride());
 
 //app.use(favicon());
 //app.use(csrf());
+//var sessionStore = require('./secrets/db.sessionStore');
+//var MongoStore = require('connect-mongo')(express);
 //app.use(session({
 //    secret: sessionStore.secret,
 //    maxAge: sessionStore.maxAge,
@@ -67,17 +60,15 @@ app.use(methodOverride());
 //    })
 //}));
 //app.use(flash());
-// app.use(app.router);
+
 if (process.env.NODE_ENV === 'development') {
     app.use(errorHandler());
 }
 
-/** Routes */
+/** Routes **/
 require('./server/apps/app.routes')(app, express);
 
-/** Server Action */
-
-
+/** Server Action **/
 var server = http.createServer(app).listen(app.get('port'), function () {
     console.log('ENV: ' + process.env.NODE_ENV);
     console.log('WWW: ' + config_host.protocol + ':' + config_host.uri + ((config_host.port != 80) ? ':' + config_host.port : ''));
@@ -85,8 +76,7 @@ var server = http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port') + " in " + app.settings.env);
 });
 
-/** Server Error Handling *//*
-
+/** Server Error Handling **/
 server.on('request', function (req, res) {
     var error_handler_domain = domain.create();
     error_handler_domain.add(req);
@@ -94,4 +84,4 @@ server.on('request', function (req, res) {
     error_handler_domain.on('error', function (err) {
         console.log(err.message);
     });
-});*/
+});
